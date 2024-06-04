03bfe3e08007debee563ae81cc97d1251ebf7561
Roshan8026 <Roshan8026>
2024-06-04 08:16:51 +0530

    ui in progress

--- src/component/MyComponent.jsx
+++ src/component/MyComponent.jsx
@@ -50,19 +50,78 @@ const MyComponent = () => {
   };
 
   return (
-    <div className="my-component">
-        <div className="button-row-container">
-                                
-                                    <Button variant="primary" type="submit" className="btn-signup"
-                                                  onClick={() => navigate('/withdraw-password')}
-                                                  >
-                                        Recharge
-                                    </Button>
-                                    <Button variant="primary" type="submit" className="btn-signup"
-                                     onClick={() => navigate('/withdraw')}>
-                                        Withdraw
-                                    </Button>
-                                </div>
+    <>
+   
+
+    <div className="my-component"> <Navbar bg="primary" variant="dark" className="fixed-top">
+                <Navbar.Brand href="#">Account Center </Navbar.Brand>
+            </Navbar>
+   <br/>
+   <br/>
+   <Card>
+   <Card.Body className="card-body-container">
+      <div className="icon-container">
+        <FaUser className="user-icon" />
+      </div>
+      <div className="text-container">
+        <p className="user-id">78788</p>
+        <p className="other-info">444</p>
+      </div>
+    </Card.Body>
+    <div className="button-row-container">    
+            <div className="text-container">
+                <p className="user-id">0</p>
+                <p className="other-info">Recharge</p>
+              </div>
+               <div className="text-container">
+                <p className="user-id">0</p>
+                <p className="other-info">Balance</p>
+              </div>
+        </div>
+        <div className="button-row-container">    
+            <div className="text-container">
+                <p className="user-id">0</p>
+                <p className="other-info">Recharge</p>
+              </div>
+               <div className="text-container">
+                <p className="user-id">0</p>
+                <p className="other-info">Balance</p>
+              </div>
+              <div className="text-container">
+                <p className="user-id">0</p>
+                <p className="other-info">Balance</p>
+              </div>
+              
+        </div>
+        <div className="button-row-container">    
+            <div className="text-container">
+                <p className="user-id">0</p>
+                <p className="other-info">Recharge</p>
+              </div>
+               <div className="text-container">
+                <p className="user-id">0</p>
+                <p className="other-info">Balance</p>
+              </div>
+              <div className="text-container">
+                <p className="user-id">0</p>
+                <p className="other-info">Balance</p>
+              </div>
+              
+        </div>
+                                </Card>
+   <br/>
+
+        <div className="button-row-container">    
+            <Button variant="primary" type="submit" className="btn-signup"
+                          onClick={() => navigate('/withdraw-password')}
+                          >
+                Recharge
+            </Button>
+            <Button variant="primary" type="submit" className="btn-signup"
+              onClick={() => navigate('/withdraw')}>
+                Withdraw
+            </Button>
+        </div>
       <div className="accordion">
         {accordionItems.map(item => (
           <div className="accordion-item" key={item.id}>
@@ -85,6 +144,8 @@ const MyComponent = () => {
         ))}
       </div>
     </div>
+     
+    </>
   );
 };
 
