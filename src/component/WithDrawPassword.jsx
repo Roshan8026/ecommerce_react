dfbf0691a88cb95ac4b5e9b8a4635bd1d78f0c25
Roshan8026 <roshanshrivastava04121999@gmail.com>
2024-11-06 22:03:15 +0530

    withdraw password is done

--- src/component/WithDrawPassword.jsx
+++ src/component/WithDrawPassword.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import "./Home.css";
 import {
   Navbar,
   Container,
@@ -9,228 +10,151 @@ import {
   Button,
 } from "react-bootstrap";
 import "./SignupPage.css"; // Import your CSS file
-import { NavLink } from "react-router-dom"; // Import NavLink from react-router-dom
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
 
+const spanStyle = {
+  padding: "20px",
+  background: "#efefef",
+  color: "#000000",
+};
+
+const divStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundSize: "cover",
+  height: "400px",
+};
+
 const WithDrawPassword = () => {
-          const navigate = useNavigate();
-  // State to manage the countdown timer
-  const [countdown, setCountdown] = useState(0);
-  // Text for the send button
-  const [sendButtonText, setSendButtonText] = useState("Send");
-  // State for form fields
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [verificationCode, setVerificationCode] = useState("");
-  // State to manage the disabled state of buttons
-  const [isSendButtonDisabled, setIsSendButtonDisabled] = useState(false);
-  const [isSignupButtonDisabled, setIsSignupButtonDisabled] = useState(true);
+    const navigate = useNavigate();
+    let token = localStorage.getItem("token");
+    let user = localStorage.getItem("user");
 
-  useEffect(() => {
-    let intervalId;
+  const [formData, setFormData] = useState({
+    newPassword: "",
+    againPassword: "",
+  });
 
-    if (countdown > 0) {
-      console.log("countdown", countdown);
-      // Set the button text to the countdown number
-      setSendButtonText(`${countdown}`);
-      // Decrease countdown every second
-      intervalId = setInterval(() => {
-        setCountdown((prevCountdown) => prevCountdown - 1);
-      }, 1000);
-    } else {
-      // Reset the button text to "Send" when countdown finishes
-      setSendButtonText("Send");
-      setIsSendButtonDisabled(false);
-    }
-
-    // Cleanup interval on component unmount or countdown end
-    return () => clearInterval(intervalId);
-  }, [countdown]);
-
- const handleSendClick = async () => {
-      if(email == '' || email == null) {
-        toast.error("please enter email");
-          return;
-      }
-     // Validate password match
-    if (password !== confirmPassword) {
-        toast.error("Passwords do not match");
-        return;
-    }
-
-    if (countdown === 0) {
-        toast.success("OTP sent successfully!");
-       // Simulate API call for sending verification code
-       const sendCodeResponse = await sendVerificationCodeAPI(email,password);
-      setCountdown(60);
-      setIsSendButtonDisabled(true);
-      setIsSignupButtonDisabled(true);
-      if (sendCodeResponse.status === 200) {
-        // Enable the Verification Code input and Signup button on success
-        setIsSignupButtonDisabled(false);
-      } else {
-        // Handle failure case
-        setIsSendButtonDisabled(false);
-        setIsSignupButtonDisabled(true);
-      }
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
-  const sendVerificationCodeAPI = async () => {
-    // Simulate API call for sending verification code
+
+  const withdrawPassword = async (email) => {
+    // Simulate API call for withdrawPassword
     try {
-      // Example of actual API call
-      const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/reset_password`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email:email, password:password }),
-      });
-      console.log('sendVerificationCodeAPI response ', response);
+       const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/withdraw_password`, {
+            method: "PUT",
+            headers: {
+            "Content-Type": "application/json",
+            "Authorization": `${token}` // Replace `yourToken` with the actual token variable
+            },
+            body: JSON.stringify({ 
+                email: email,
+                password: formData.newPassword,
+            }),
+        });
+      console.log('withdrawPassword response', response)
       return response;
     } catch (error) {
-      console.error("Failed to send verification code", error);
-      toast.error("Failed to send OTP. Please try again.");
+      console.error("withdrawPassword failed", error);
+      toast.error("withdrawPassword failed. Please try again.");
       return { status: 500 }; // Return a failure status
     }
   };
 
-   const handleSignupClick = async (e) => {
-        e.preventDefault();
-        setIsSignupButtonDisabled(true);
 
-        // Simulate API call for signup
-        const signupResponse = await signupAPI();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let userDetail = JSON.parse(user);
+    console.log('userDetail',userDetail.user.email)
+    if(formData.newPassword == '' || formData.againPassword == '') {
+          toast.error("please enter valid input");
+          return;
+      }
+     // Validate password match
+    if (formData.newPassword !== formData.againPassword) {
+        toast.error("Passwords do not match");
+        return;
+    }
 
-        if (signupResponse.status === 200) {
-            toast.success('verification is successfully Please login');
-            setTimeout(() => {
-                navigate('/login');
-            },2000)
-        } else {
-            toast.success("Signup failed");
-        setIsSignupButtonDisabled(false);
-        }
-    };
+    console.log("Form Data:", formData);
+    // Simulate API call for signup
+    const Response = await withdrawPassword(userDetail.user.email);
 
-    const signupAPI = async () => {
-        // Simulate API call for signup
-        try {
-        const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/verify_otp`, {
-            method: "POST",
-            headers: {
-            "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ email:email, otp:verificationCode }),
-        });
-        console.log('signupAPI response', response)
-        return response;
-        } catch (error) {
-        console.error("Signup failed", error);
-        toast.error("verification failed. Please try again.");
-        return { status: 500 }; // Return a failure status
-        }
-    };
+    if (Response.status === 200) {
+        toast.success('withdraw Password is successfull');
+        setTimeout(() => {
+            navigate('/home');
+        },1000)
+    } else {
+        toast.error("withdraw Password failed");
+    }
+
+    // Here you can handle the form submission, e.g., sending the data to a server
+  };
 
   return (
-    <div>
+    <>
       <Navbar bg="primary" variant="dark" className="fixed-top">
         <Navbar.Brand href="#" className="px-4">
-          PC Health App
+          Withdraw Password
         </Navbar.Brand>
       </Navbar>
 
-      <div className="container mt-5 pt-4">
-        <ToastContainer />
-        <div className="row">
-          <div className="col-12">
-            <Card className="reset-password-card">
-              <Card.Body>
-                <h2 style={{ fontWeight: "bold", marginBottom: "15px" }}>
-                  {" "}
-                  WithDraw Password{" "}
-                </h2>
-                <Form>
-                  <Row className="mb-3">
-                    <Col>
-                       <Form.Control
-                        type="email"
-                        placeholder="Please Enter Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="form-control"
-                      />
-                    </Col>
-                  </Row>
-                  <Row className="mb-3">
-                    <Col>
-                       <Form.Control
-                        type="password"
-                        placeholder="Please input Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="form-control"
-                      />
-                    </Col>
-                  </Row>
-                  <Row className="mb-3">
-                    <Col>
+      <section className="mt-5 pt-5">
+        <div className="container">
+            <ToastContainer />
+          <div className="row">
+            <div className="col-md-12">
+              <Card
+                className="reset-password-card card "
+                style={{ width: "154%" }}
+              >
+                <Card.Body>
+                  <h3>Withdraw Password </h3>
+                  <br />
+                  <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="newPassword">
+                      <Form.Label>WithDraw New Password</Form.Label>
                       <Form.Control
-                        type="password"
-                        placeholder="Please enter password again"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        className="form-control"
-                      />
-                    </Col>
-                  </Row>
-                  <Row className="mb-3">
-                    <Col xs={8}>
-                       <Form.Control
                         type="text"
-                        placeholder="Verification Code"
-                        value={verificationCode}
-                        onChange={(e) => setVerificationCode(e.target.value)}
-                        disabled={isSignupButtonDisabled}
-                        className="form-control"
+                        placeholder="Please Enter Withdraw New Password"
+                        name="newPassword"
+                        value={formData.newPassword}
+                        onChange={handleChange}
                       />
-                    </Col>
-                    <Col xs={4}>
-                     <Button
-                        variant="primary"
-                        type="button"
-                        disabled={isSendButtonDisabled}
-                        onClick={handleSendClick}
-                        className="btn-send"
-                        style={{ height: "73%" }}
-                      >
-                        {sendButtonText}
-                      </Button>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col>
-                      <Button
-                        variant="primary"
-                        type="submit"
-                        className="btn-signup"
-                        disabled={isSignupButtonDisabled}
-                        onClick={handleSignupClick}
-                      >
-                        Confirm
-                      </Button>
-                    </Col>
-                  </Row>
-                </Form>
-              </Card.Body>
-            </Card>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="againPassword">
+                      <Form.Label>Withdraw Re-enter Password</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Please enter withdraw new Password Again"
+                        name="againPassword"
+                        value={formData.againPassword}
+                        onChange={handleChange}
+                      />
+                    </Form.Group>
+
+                    <br />
+                    <Button variant="primary" type="submit">
+                      confirm
+                    </Button>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 };
 
